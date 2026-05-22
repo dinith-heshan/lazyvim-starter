9d44cced93428f190b06813bb94769933e8582b1
dinith-heshan <dinithheshansp@gmail.com>
2026-05-22 11:47:33 +0530

    refactor: local repo init for lazy vim

--- init.lua
+++ init.lua
@@ -1,2 +1,6 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+if vim.g.vscode then
+  -- minimal Neovim (no UI plugins)
+else
+  -- bootstrap lazy.nvim, LazyVim
+  require("config.lazy")
+end
